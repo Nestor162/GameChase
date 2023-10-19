d1805ad56cb86ab7c1db101989ec89d5908bb6fc
Nestor162 <nestorcicardini@gmail.com>
2023-10-19 12:02:25 +0200

    uninstall tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
 import NavBar from './components/NavBar'
-import './app.css'
 import GameGrid from './components/GameGrid'
 
 function App() {
